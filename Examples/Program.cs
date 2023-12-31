6d903c470f1a21041958a93991cfb9da3466ee73
Jon Nuttall <jonathan.nut@gmail.com>
2023-12-31 00:21:47 +0000

    feat: created Examples project

---
+++ Examples/Program.cs
@@ -0,0 +1,16 @@
+﻿using D2;
+using D2.Enums;
+
+var A = new Shape("A", "Starting Point");
+var B = new Shape("B", "Ending Point");
+var C = new Shape("C", "Middle Point", ShapeType.Hexagon);
+
+var basicTemplate = Diagram.Create()
+    .Add(A)
+    .Add(B)
+    .Add(C)
+    .Add(new Connection(A, C))
+    .Add(new Connection(C, B, "Hello World!"));
+
+// Each component overrides ToString() to return the D2 code for that instance
+Console.WriteLine(basicTemplate);
