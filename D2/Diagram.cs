4086f4a2b1bdbb8e1394c84ff9ca7c11e20da5bc
Jon Nuttall <jonathan.nut@gmail.com>
2024-01-02 15:08:32 +0000

    feat: Added CreateShape/Connection to Diagram as helper methods

--- D2/Diagram.cs
+++ D2/Diagram.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using D2.Enums;
 using D2.Interfaces;
 
 namespace D2
@@ -20,6 +21,18 @@ namespace D2
         public Diagram Add(IEnumerable<IRenderable> renderable)
         {
             _renderables.AddRange(renderable);
+            return this;
+        }
+
+        public Diagram CreateShape(string key, string label, ShapeType type)
+        {
+            _renderables.Add(new Shape(key, label, type));
+            return this;
+        }
+        
+        public Diagram CreateConnection(string from, string to, string label = "", ArrowheadOptions? arrowhead = null)
+        {
+            _renderables.Add(new Connection(from, to, label, arrowhead));
             return this;
         }
 
